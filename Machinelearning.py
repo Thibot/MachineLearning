b14d0befb589ce4d95c9c048c221df0da62ec4b1
Bastien Laise <bastien.laise@hotmail.fr>
2018-04-24 00:54:57 +0200

    output

--- Machinelearning.py
+++ Machinelearning.py
@@ -8,6 +8,18 @@ Created on Mon Apr 23 21:49:39 2018
 import pandas as pd
 
 def main():
-    data = pd.read_csv("./Data/DataSet.csv")
-
+    data = pd.read_csv("./Data/DataSet.csv", header = 0)
+    outputContinus(data)
+    
+def outputContinus(data):
+    #continusValue = pandas.DataFrame
+    i = 0
+    for column in data:
+        if i in [1, 3, 11, 12, 13]:
+            print(data[i])
+        i += 1
+    
+def outputCategorical(data):
+    print()
+    
 main()
