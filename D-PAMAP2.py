56a88c4a15a1574ac4ae24354406f4901cd4d58d
Thibaut <thibaut.roudel@etudiant.univ-rennes1.fr>
2018-04-25 22:54:04 +0100

    ReadData

--- D-PAMAP2.py
+++ D-PAMAP2.py
@@ -4,16 +4,11 @@ Created on Wed Apr 25 20:00:54 2018
 
 @author: Thibaut
 """
-import numpy as np
+import D_DataFormatting
 
 
 def main():
     
     
-    myarray = np.fromfile('./Data/PAMAP2_Dataset/Protocol/subject101.dat',dtype=float,count=55,sep=" ")
-    
-    for i in len(myarray):
-        
-    
-    print(myarray)
+
 main()
---
+++ D_DataFormatting.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 22:46:03 2018
+
+@author: Thibaut
+"""
+
+import numpy as np
+
+#Function to read a .dat file and to return a double dimensions tab
+#nameFile = path of the file
+def readDATFile(nameFile):
+    myarray = np.fromfile(nameFile,dtype=float,sep=' ')
+    newArray = []
+    ligne=[]
+    for i in range(0,len(myarray)):
+        ligne.append(myarray[i])
+        if(i%54==0 and i!=0):
+            newArray.append(ligne)
+            ligne=[]
+
+    return newArray
