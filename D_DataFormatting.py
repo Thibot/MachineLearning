4253febd269f8a8a81a2a79c163619c5f001a9ed
Thibaut <thibaut.roudel@etudiant.univ-rennes1.fr>
2018-04-26 10:25:36 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- D_DataFormatting.py
+++ D_DataFormatting.py
@@ -5,28 +5,12 @@ Created on Wed Apr 25 22:46:03 2018
 @author: Thibaut
 """
 
-import numpy as np
 import pandas as pd
-
-#Function to read a .dat file and to return a double dimensions tab
-#nameFile = path of the file
-def readDATFile(nameFile):
-    myarray = np.fromfile(nameFile,dtype=float,sep=' ')
-    newArray = []
-    ligne=[]
-    for i in range(0,len(myarray)):
-        ligne.append(myarray[i])
-        if(i%54==0 and i!=0):
-            newArray.append(ligne)
-            ligne=[]
-
-    return newArray
 
 #Function deleting a column in a dataframe
 #DataFrame = dataframe object
 #ColumnName = Name of the column to delete
-def delColumn(DataFrame,ColumnName):
-    DataFrame.drop(ColumnName, axis=1, inplace=True)
+#def delColumn(DataFrame,ColumnName):
     
 #Function deleting a row in a dataframe
 #DataFrame = dataframe object
@@ -35,12 +19,8 @@ def delRow(DataFrame,ColumnName, value):
     DataFrame = DataFrame[DataFrame[ColumnName] != value]
     return DataFrame
 
-#Function to save in csv values of dat file
-#DataFrame = an array of array with values
-#fileName  = name of the file
-#columns   = array of columns you want to export
-def toCSV(values, fileName, columns):
-    df = pd.DataFrame(values,
-              columns = columns)
+def DATtoCSV(OriginFile,fileName,Columns):
     
-    df.to_csv("./" + fileName + ".csv")
+    data = pd.read_csv(OriginFile,sep='\s+',names=Columns)
+    
+    data.to_csv(fileName,columns=Columns)
