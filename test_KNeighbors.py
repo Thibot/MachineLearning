18ab3e89dd1a0d4eaf9a11f5ed212c5eb2472edc
Bastien Laise <bastien.laise@hotmail.fr>
2018-04-26 21:59:51 +0100

    end neighbors

--- test_KNeighbors.py
+++ test_KNeighbors.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-from sklearn.model_selection import cross_val_score
-from sklearn import tree
+from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report, confusion_matrix 
 from sklearn.neighbors import KNeighborsClassifier  
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix  
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn import metrics
 import pandas as pd
-import graphviz as gr
-import numpy as np
 
 def main():
     
@@ -18,42 +16,22 @@ def main():
     X = data.drop(['Class'], axis=1)  
     y = data['Class']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)  
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)  
     
     classifier = KNeighborsClassifier(n_neighbors=5)  
     classifier.fit(X_train, y_train) 
     
     y_pred = classifier.predict(X_test)
     
-    print(confusion_matrix(y_test, y_pred))  
-    print(classification_report(y_test, y_pred))  
+    #print(confusion_matrix(y_test, y_pred))  
+    #print(classification_report(y_test, y_pred))  
     
-    # creating odd list of K for KNN
-    myList = list(range(1,50))
+    print(accuracy_score(y_test, y_pred))
     
-    # subsetting just the odd ones
-    neighbors = filter(lambda x: x % 2 != 0, myList)
+    scores =  cross_val_score(classifier, X_train, y_train, cv = 5)
+    print((scores))
     
-    # empty list that will hold cv scores
-    cv_scores = []
-    
-    # perform 10-fold cross validation
-    for k in neighbors:
-        knn = KNeighborsClassifier(n_neighbors=k)
-        scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
-        cv_scores.append(scores.mean())
-        
-    # changing to misclassification error
-    MSE = [1 - x for x in cv_scores]
-    
-    # determining best k
-    optimal_k = neighbors[MSE.index(min(MSE))]
-    print ("The optimal number of neighbors is %d" % optimal_k)
-    
-    # plot misclassification error vs k
-    plt.plot(neighbors, MSE)
-    plt.xlabel('Number of Neighbors K')
-    plt.ylabel('Misclassification Error')
-    plt.show()
+    #predicted =  cross_val_predict(classifier, X, y, cv = 20)
+    #print(metrics.accuracy_score(y, predicted))
     
 main()
