00f93f019160568b5cb29c5b056946f99b98fe4a
Hemery Florian <florian.hemery@etudiant.univ-rennes1.fr>
2018-04-27 00:24:29 +0100

    cross test arrays and comment

--- modelsGenerator.py
+++ modelsGenerator.py
@@ -6,79 +6,97 @@ Created on Thu Apr 26 22:55:19 2018
 """
 from sklearn import tree
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.metrics import classification_report, confusion_matrix  
-from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import datasets
 
-import pandas as pd
-import graphviz as gr
+# LIBRARY FOR GENERATE THE TREE VISUALISATION
+# import graphviz as gr
 
 #make the model of the dataset with the random forest technique
 def randomForest(data, target, cv):
+    # getting array of imput values
     X = data.drop([target], axis=1)   
+    # getting array of target values 
     y = data[target] 
     
+    # getting training array and test array
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     
+    # initialise the random forest model
     clf = RandomForestClassifier(max_depth=2, random_state=0)
     
+    # generate model with training array
     clf.fit(X_train, y_train)
     
+    # predict target from test array
     y_pred = clf.predict(X_test)
     
     print("Confusion matrix : \n", confusion_matrix(y_test, y_pred))   
     print("Classification report : \n", classification_report(y_test, y_pred))
     if (cv != 0):
         print("Accuracy score : \n", accuracy_score(y_test, y_pred))
-        print("Cross validation score : \n", cross_val_score(clf, X, y, cv = cv))
+        print("Cross validation score : \n", cross_val_score(clf, X_test, y_test, cv = cv))
     
     return clf
 
 #make the model of the dataset with the random forest technique and entropy gestion
 def randomForestWithEntropy(data, target, cv):
-    X = data.drop([target], axis=1)   
+    # getting array of imput values
+    X = data.drop([target], axis=1)  
+    # getting array of target values 
     y = data[target] 
     
+    # getting training array and test array
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     
+    # initialise the random forest model with entropy
     clf = RandomForestClassifier(max_depth=2, random_state=0, criterion = "entropy")
     
+    # generate model with training array
     clf.fit(X_train, y_train)
     
+    # predict target from test array
     y_pred = clf.predict(X_test)
     
     print("Confusion matrix : \n", confusion_matrix(y_test, y_pred))   
     print("Classification report : \n", classification_report(y_test, y_pred))
     if (cv != 0):
         print("Accuracy score : \n", accuracy_score(y_test, y_pred))
-        print("Cross validation score : \n", cross_val_score(clf, X, y, cv = cv))
+        print("Cross validation score : \n", cross_val_score(clf, X_test, y_test, cv = cv))
     
     return clf
 
 #make the model of the dataset with the decison tree technique
 def decisionTree(data, target, cv):
+    # getting array of imput values
     X = data.drop([target], axis=1)  
+    # getting array of target values 
     y = data[target]
     
+    # getting training array and test array with a training size of 0.5
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
         test_size=0.50
     )
     
+    # initialise the decision tree model
     clf = tree.DecisionTreeClassifier()
+    
+    # generate model with training array
     clf.fit(X=X_train, y=y_train)
+    
+    # predict target from test array
     y_pred = clf.predict(X_test)
 
     print("Confusion matrix : \n", confusion_matrix(y_test, y_pred))    
     print("Classification report : \n", classification_report(y_test, y_pred))
     if (cv != 0):
         print("Accuracy score : \n", accuracy_score(y_test, y_pred))
-        print("Cross validation score : \n", cross_val_score(clf, X, y, cv = cv))
+        print("Cross validation score : \n", cross_val_score(clf, X_test, y_test, cv = cv))
 
+    # -- GENERATE A PDF FOR THE CREATED DECISION TREE --
     #decision_tree = tree.export_graphviz(clf, out_file=None) 
     #graph = gr.Source(decision_tree) 
     #graph.render("decision_tree")
@@ -87,26 +105,34 @@ def decisionTree(data, target, cv):
 
 #make the model of the dataset with the decison tree technique and entropy gestion
 def decisionTreeWithEntropy(data, target, cv):
-    
-    X = data.drop([target], axis=1)  
+    # getting array of imput values
+    X = data.drop([target], axis=1)
+    # getting array of target values 
     y = data[target]
     
+    # getting training array and test array with a training size of 0.5
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
         test_size=0.50
     )
     
+    # initialise the decision tree model with entropy
     clf = tree.DecisionTreeClassifier(criterion = "entropy")
+    
+    # generate model with training array
     clf.fit(X=X_train, y=y_train)
+    
+    # predict target from test array
     y_pred = clf.predict(X_test)
 
     print("Confusion matrix : \n", confusion_matrix(y_test, y_pred))    
     print("Classification report : \n", classification_report(y_test, y_pred)) 
     if (cv != 0):
         print("Accuracy score : \n", accuracy_score(y_test, y_pred))
-        print("Cross validation score : \n", cross_val_score(clf, X, y, cv = cv))
+        print("Cross validation score : \n", cross_val_score(clf, X_test, y_test, cv = cv))
 
+    # -- GENERATE A PDF FOR THE CREATED DECISION TREE --
     #decision_tree = tree.export_graphviz(clf, out_file=None) 
     #graph = gr.Source(decision_tree) 
     #graph.render("decision_tree_entropy")
@@ -115,21 +141,27 @@ def decisionTreeWithEntropy(data, target, cv):
     
 #make the model of the dataset with the decison nearest neighbors technique
 def nearestNeighbors(data, target, cv):
-
-    X = data.drop([target], axis=1)  
+    # getting array of imput values
+    X = data.drop([target], axis=1)
+    # getting array of target values 
     y = data[target]
     
+    # getting training array and test array with a training size of 0.5
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.50)  
     
-    clf = KNeighborsClassifier(n_neighbors=5)  
+    # initialise the k-nearest neighbors model
+    clf = KNeighborsClassifier(n_neighbors=5)
+    
+    # generate model with training array
     clf.fit(X_train, y_train) 
     
+    # predict target from test array
     y_pred = clf.predict(X_test)    
 
     print("Confusion matrix : \n", confusion_matrix(y_test, y_pred))   
     print("Classification report : \n", classification_report(y_test, y_pred))
     if (cv != 0):
         print("Accuracy score : \n", accuracy_score(y_test, y_pred))
-        print("Cross validation score : \n", cross_val_score(clf, X, y, cv = cv))
+        print("Cross validation score : \n", cross_val_score(clf, X_test, y_test, cv = cv))
     
     return clf
